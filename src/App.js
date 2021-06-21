a23237f7e480f5f2cb21ad41853f148095366884
Aakash Niwane <aakash.niwane@spit.ac.in>
2021-06-21 23:20:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Learn Dance' />
+      <Todo text='Learn Singing' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
+function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    function deleteHandler() {
+        setModalIsOpen(true);
+    }
+
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
+
+    return (
+        <div className='card'>
+            <h2>{props.text}</h2>
+            <div className='actions'>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
+            </div>
+            { modalIsOpen && <Modal text={props.text} onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+            { modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+        </div>
+    );
+}
+
+export default Todo;
